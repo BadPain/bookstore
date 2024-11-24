4e90f47ad36d92b76ba47779efff566a764833f9
IcePain <143926986+BadPain@users.noreply.github.com>
2024-11-24 19:15:32 +0100

    book template reworked

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,26 @@ function init() {
     renderBooks();
 }
 
-
 function renderBooks() {
     let booksContent = document.getElementById('content');
-
-    for (let i = 0; i < books.length; i++) {
+    for (let indexBook = 0; indexBook < books.length; indexBook++) {
+        booksContent.innerHTML += getBookTemplate(indexBook);
     };
-    booksContent.innerHTML +=
-        `<section class="bookViewClass">
+}
 
-        <div class="book_Name">${books[1].name}</div>
+function getBookTemplate(indexBook) {
+    return `<section class="bookViewClass">
+
+        <div class="book_Name">${books[indexBook].name}</div>
         <hr>
         <div class="book_Icon_Class">
             <img class="book_Icon" src="./img/icons/book_icon.svg" alt="">
         </div>
         <hr>
         <div class="book_Liked">
-            <p class="book_Price">${books[1].price}</p>
+            <p class="book_Price">${books[indexBook].price}</p>
             <div class="book_New_Liked">
-                <p>${books[1].likes}</p>
+                <p>${books[indexBook].likes}</p>
                 <img class="book_heart" src="./img/icons/heart_on.svg" alt="">
                 <img class="book_heart" src="./img/icons/heart_off.svg" alt="">
             </div>
@@ -32,15 +33,24 @@ function renderBooks() {
                 <span>Genre</span>
             </div>
             <div class="book_Infos_Script">
-                <p>: ${books[1].author}</p>
-                <p>: ${books[1].publishedYear}</p>
-                <p>: ${books[1].genre}</p>
+                <p>: ${books[indexBook].author}</p>
+                <p>: ${books[indexBook].publishedYear}</p>
+                <p>: ${books[indexBook].genre}</p>
             </div>
         </div>
         <hr>
         <div>
-            <p class="comment" id="comment_section">Kommentare:</p>
-            <p>${books[1].comments}</p>
+            
         </div>
-    </section>`;
-}  
+    </section>
+    <br>
+    <br>`
+}
+
+// function getCommentTemplate(indexBook, indexComment) {
+//     return `
+//     <p class="comment" id="comment_section">Kommentare:</p>
+//     <p>${books[indexBook].comments[indexComment].name}</p>
+//     <p>${books[indexBook].comments[indexComment].comment}</p>
+//     `
+// }
